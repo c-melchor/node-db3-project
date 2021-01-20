821cc53812822d1e68e9aeabf0522dc5aa7ec762
c_melchor <cah.melchor@gmail.com>
2021-01-20 13:34:14 -0800

    add(scheme) works

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,25 @@
 // scheme-model
+const db = require("../../data/db-config");
+
+module.exports = {
+  find() {
+    return db("schemes");
+  },
+  findById(id) {
+    return db("schemes")
+      .where("id", id)
+      .first();
+  },
+  //   findSteps(id) {
+  //     return db("schemes").join("steps", "id", "scheme_id");
+  //   },
+  add(scheme) {
+    return db("schemes")
+      .insert(scheme)
+      .then(([scheme_id]) => {
+        return db("schemes")
+          .where("id", scheme_id)
+          .first();
+      });
+  }
+};
