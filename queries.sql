f6f9f3b614b5ffc58b99670e45c7142bfe4e0e7e
c_melchor <cah.melchor@gmail.com>
2021-01-21 08:26:27 -0800

    updated sql query

--- queries.sql
+++ queries.sql
@@ -8,9 +8,12 @@ select ProductName, CategoryName from product as p
 
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-select o.id, s.CompanyName, o.orderdate from [order] as o
+select o.id, s.CompanyName from [order] as o
     join shipper as s
-    where o.orderdate < '2012-08-09'
+    on o.ShipVia = s.Id
+    where o.OrderDate < '2012-08-09'
+    
+
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
